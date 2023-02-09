c90507366d684edbf13ea225f4ede3dc550dad8c
Pietro Niero Roque <pietro.roque@trinusco.com.br>
2023-02-09 18:04:25 -0300

    Correções UserItem.

--- src/components/items/UserItem.js
+++ src/components/items/UserItem.js
@@ -2,28 +2,28 @@ import "./BaseItem.css";
 
 const UserItem = ({ item }) => {
   return (
-    <div className="item-container">
+    <div className="item-container" style={{width: "200px"}}>
       <div className="item-content-header">
         <p
           className="subheader"
           style={{ fontWeight: "550", marginRight: "4px" }}
         >
-          {item.name})
+          {item.name}
         </p>
-        <div style={{ display: "flex" }}>
-          <div style={{ display: "block", marginRight: "8px" }}>
-            <p className="subheader" style={{ marginBottom: "4px" }}>
+        <div style={{ display: "flex", marginTop: "8px" }}>
+          <div style={{ display: "block", marginRight: "36px" }}>
+            <p className="body-medium" style={{ marginBottom: "4px" }}>
               Data de nascimento
             </p>
-            <p className="body" style={{ fontSize: 12 }}>
-              {item.birthData}
+            <p className="body">
+              {item.birthDate}
             </p>
           </div>
           <div style={{ display: "block" }}>
-            <p className="subheader" style={{ marginBottom: "4px" }}>
+            <p className="body-medium" style={{ marginBottom: "4px" }}>
               Sexo
             </p>
-            <p className="body" style={{ fontSize: 12 }}>
+            <p className="body">
               {item.sex}
             </p>
           </div>
@@ -31,45 +31,45 @@ const UserItem = ({ item }) => {
       </div>
       <div className="item-content-body">
         <div style={{ display: "flex" }}>
-          <div style={{ display: "block", marginRight: "8px" }}>
-            <p className="body" style={{ marginBottom: "4px" }}>
+          <div style={{ display: "block", marginRight: "24px" }}>
+            <p className="body-medium" style={{ marginBottom: "4px" }}>
               Endereço
             </p>
-            <p className="metadata" style={{ fontSize: 12 }}>
+            <p className="body">
               {item.address}
             </p>
           </div>
-          <div style={{ display: "block", marginRight: "8px" }}>
-            <p className="body" style={{ marginBottom: "4px" }}>
+          <div style={{ display: "block", marginRight: "24px" }}>
+            <p className="body-medium" style={{ marginBottom: "4px" }}>
               Número
             </p>
-            <p className="metadata" style={{ fontSize: 12 }}>
+            <p className="body">
               {item.number}
             </p>
           </div>
           <div style={{ display: "block" }}>
-            <p className="body" style={{ marginBottom: "4px" }}>
+            <p className="body-medium" style={{ marginBottom: "4px" }}>
               Setor
             </p>
-            <p className="metadata" style={{ fontSize: 12 }}>
+            <p className="body">
               {item.sector}
             </p>
           </div>
         </div>
-        <div style={{ display: "flex" }}>
+        <div style={{ display: "flex", marginTop: "8px" }}>
           <div style={{ display: "block", marginRight: "8px" }}>
-            <p className="body" style={{ marginBottom: "4px" }}>
+            <p className="body-medium" style={{ marginBottom: "4px" }}>
               Cidade
             </p>
-            <p className="metadata" style={{ fontSize: 12 }}>
+            <p className="body">
               {item.city}
             </p>
           </div>
           <div style={{ display: "block" }}>
-            <p className="body" style={{ marginBottom: "4px" }}>
+            <p className="body-medium" style={{ marginBottom: "4px" }}>
               Estado
             </p>
-            <p className="metadata" style={{ fontSize: 12 }}>
+            <p className="body">
               {item.uf}
             </p>
           </div>
@@ -79,7 +79,7 @@ const UserItem = ({ item }) => {
             <p className="body-medium">Alergias:</p>
             {item.allergies.map((allergyItem, index) => {
               return (
-              <div style={{display: "flex"}}>
+              <div style={{display: "flex"}} key={index}>
                   <p className="body" style={{ margin: "0px 8px 0px 0px" }}>
                     {index})
                   </p>
